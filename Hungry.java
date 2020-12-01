fd03ebce60d85f3a14bf085ec60979ff4b9595e8
Sandeepreddy99 <sandeepreddy.msrmsr@gmail.com>
2020-12-01 15:23:33 +0530

    Modified code

--- Hungry.java
+++ Hungry.java
@@ -4,18 +4,19 @@ class Hungry
 public static void main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
+System.out.println("Enter stomach status");
 String Stomach=sc.nextLine();
-if(Stomach.equals(Empty))
+if(Stomach.equals("Empty"))
 {
 System.out.println("Yeah am hungry");
 }
-else if(Stomach.equals(Ok))
+else if(Stomach.equals("Not Hungry"))
 {
 System.out.println("No am ok");
 }
 else
 {
-System.out.println("I am not hungry");
+System.out.println("I am Full");
 }
 }
 }
